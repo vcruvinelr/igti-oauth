c0b8037759be6b5bf36348d374147a82c9473af3
Vinicius <vcruvinelr@gmail.com>
2020-07-15 20:51:32 -0300

    entrega trabalho prático

---
+++ configuration/config.js
@@ -0,0 +1,8 @@
+module.exports={
+    "api_key": "1",
+    "api_secret": "1",
+    "callback_url": "http://localhost:3000/auth/facebook/callback",
+    "api_key_google": "2",
+    "api_secret_google": "2",
+    "callback_url_google": "http://localhost:3000/auth/google/callback"
+}
